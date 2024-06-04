860e215ab677efc59eed1b74d4f03bdb411faaf6
SeanAlden <130992991+SeanAlden@users.noreply.github.com>
2024-06-04 22:32:49 +0700

    Proyek Akhir PABA - Sampai Halaman Profil, tambah activity

---
+++ app/src/main/java/project/c14210052/proyekakhir_paba/CashierActivity.kt
@@ -0,0 +1,20 @@
+package project.c14210052.proyekakhir_paba
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CashierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cashier)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
