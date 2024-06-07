8445da314e93281c8f618502ca4c20ad42bfbe8c
SeanAlden <130992991+SeanAlden@users.noreply.github.com>
2024-06-08 01:11:47 +0700

    Proyek Akhir PABA - Sabtu, 8 Juni 2024 - 01.11

--- app/src/main/java/project/c14210052/proyekakhir_paba/InventoryActivity.kt
+++ app/src/main/java/project/c14210052/proyekakhir_paba/InventoryActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ProductListActivity : AppCompatActivity() {
+class InventoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_product_list)
+        setContentView(R.layout.activity_inventory)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
