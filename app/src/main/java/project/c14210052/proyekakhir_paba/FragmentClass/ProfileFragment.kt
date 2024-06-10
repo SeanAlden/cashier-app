da78cc7adc61f57e3c7d5822c3013728622ac4e2
SeanAlden <130992991+SeanAlden@users.noreply.github.com>
2024-06-10 11:03:37 +0700

    Proyek Akhir PABA - Senin, 10 Juni 2024 - 11:03

--- app/src/main/java/project/c14210052/proyekakhir_paba/FragmentClass/ProfileFragment.kt
+++ app/src/main/java/project/c14210052/proyekakhir_paba/FragmentClass/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package project.c14210052.proyekakhir_paba.FragmentClass
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import project.c14210052.proyekakhir_paba.EditProfileActivity
 //import project.c14210052.proyekakhir_paba.ARG_PARAM1
 //import project.c14210052.proyekakhir_paba.ARG_PARAM2
 import project.c14210052.proyekakhir_paba.R
@@ -33,12 +38,19 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View? {val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val editProfileButton: Button = view.findViewById(R.id.editProfileBtn)
+
+        editProfileButton.setOnClickListener {
+            val intentEditProfile = Intent(activity, EditProfileActivity::class.java)
+            startActivity(intentEditProfile)
+        }
+        return view
     }
 
     companion object {
