a60ed8263815d03d3cd6bfc3f18927b808fa1fe3
SeanAlden <130992991+SeanAlden@users.noreply.github.com>
2024-07-04 23:36:18 +0700

    rapiin beberapa bagian code

--- app/src/main/java/project/c14210052_c14210182/proyekakhir_paba/productCategoryPage.kt
+++ app/src/main/java/project/c14210052_c14210182/proyekakhir_paba/productCategoryPage.kt
@@ -228,38 +228,3 @@ class productCategoryPage : AppCompatActivity() {
             }
     }
 }
-
-
-//package project.c14210052.proyekakhir_paba
-//
-//import android.annotation.SuppressLint
-//import android.content.Intent
-//import android.os.Bundle
-//import android.widget.ImageButton
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//
-//class productCategoryPage : AppCompatActivity() {
-//
-//    private lateinit var _backBtn : ImageButton
-//    @SuppressLint("MissingInflatedId")
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_product_category)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//
-//        _backBtn = findViewById(R.id.btnBackFromAddCategory)
-//
-//        _backBtn.setOnClickListener {
-//            val intent = Intent(this@productCategoryPage, daftarProdukPage::class.java)
-//            startActivity(intent)
-//        }
-//    }
-//}
