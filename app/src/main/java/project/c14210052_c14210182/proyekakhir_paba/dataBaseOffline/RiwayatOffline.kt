40eec9535032f0cd1cbea97d3287d718769fc212
mouritus <125249382+mouritus@users.noreply.github.com>
2024-07-06 14:10:15 +0700

    Reapply "a"
    This reverts commit 5c709cd4ef8209ae7bca262a0375fe6765e2d7d8.

---
+++ app/src/main/java/project/c14210052_c14210182/proyekakhir_paba/dataBaseOffline/RiwayatOffline.kt
@@ -0,0 +1,19 @@
+package project.c14210052_c14210182.proyekakhir_paba.dataBaseOffline
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "riwayat")
+data class RiwayatOffline(
+    @PrimaryKey @ColumnInfo(name = "id") val id: String? = null,
+    @ColumnInfo(name = "tanggal") val tanggal: String? = null,
+    @ColumnInfo(name = "waktu") val waktu: String? = null,
+    @ColumnInfo(name = "namaProduk") val namaProduk: ArrayList<String>? = null,
+    @ColumnInfo(name = "hargaPerProduk") val hargaPerProduk: ArrayList<String>? = null,
+    @ColumnInfo(name = "jumlah") val jumlah: ArrayList<String>? = null,
+    @ColumnInfo(name = "total") val total: Int = 0,
+    @ColumnInfo(name = "bayar") val bayar: Int = 0,
+    @ColumnInfo(name = "kembalian") val kembalian: Int = 0
+
+)
